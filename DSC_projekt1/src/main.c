d5ed503c3456da92bb50a24996a3f345d5d1fff5
Konrad <Konrad@DESKTOP-AGSB3CE.home>
2019-11-27 19:35:05 +0100

    Second timer configured, changing duty cycle doesn't work a/i.

--- DSC_projekt1/src/main.c
+++ DSC_projekt1/src/main.c
@@ -11,33 +11,61 @@
 
 #include "stm32l1xx.h"
 
-uint8_t i = 0;
+uint16_t i = 0;
+uint8_t dir = 1;
 
 int main(void)
 {
-
+	//Konfiguracja RCC
 	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
 	RCC -> APB1ENR |= RCC_APB1ENR_TIM2EN;
+	RCC -> APB1ENR |= RCC_APB1ENR_TIM3EN;
 
+	//Konfiguracja GPIO-PA5 - funkcja alternatywna (TIM2_CH1)
 	GPIOA -> MODER = 0xA8000000; //reset value
 	GPIOA -> MODER |= GPIO_MODER_MODER5_1;
 	GPIOA -> MODER &= ~GPIO_MODER_MODER5_0;
 	GPIOA -> AFR[0] = 0x00100000; //GPIO_AFRL_AFSEL5; --AF1 - TIM2_CH1 --NA SZTYWNO, MAKRO DO SPRAWDZENIA
 
-	TIM2 -> PSC = 50;
-	TIM2 -> ARR = 255;
-	TIM2 -> CCR1 = 50;
-	TIM2 -> CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1PE;
-	TIM2 -> CCER |= TIM_CCER_CC1E;
-	TIM2 -> EGR |= TIM_EGR_UG;
-	TIM2 -> CR1 |= TIM_CR1_CEN;
+	//Konfiguracja TIM3 - podstawa czasu (100ms ??)
+	//SystemCoreClock        = 2097000U;
+	TIM3 -> PSC = SystemCoreClock/1000; //podstawa czasu = 1ms
+	TIM3 -> ARR = 100;
+	TIM3 -> EGR |= TIM_EGR_UG;
+	TIM3 -> DIER |= TIM_DIER_CC1IE;
+	NVIC_EnableIRQ(TIM3_IRQn);
+	TIM3 -> CR1 |= TIM_CR1_CEN;
+
+	//Konfiguracja TIM2 - zegar z wyjœciem PWM
+	TIM2 -> PSC = 100;
+	TIM2 -> ARR = 10000;
+	TIM2 -> CCR1 = 100;
+	TIM2 -> CR1 |= TIM_CR1_ARPE;
+	TIM2 -> CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1PE;
+	TIM2 -> CCER |= TIM_CCER_CC1E; //enable channel 1
+	TIM2 -> EGR |= TIM_EGR_UG; //co to by³o, event generator???
+	TIM2 -> CR1 |= TIM_CR1_CEN; //enable timer
 
 	while (1)
 	{
-		TIM2 -> CCR1 = i++;
-		for (int b = 0; b < 10; b++)
-		{
 
-		}//PRYMITYWNE OPÓNIENIE
 	}
 }
+
+void TIM3_IRQHandler(void)
+{
+	TIM3->SR &= ~TIM_SR_UIF;
+	if (dir == 1)
+	{
+		TIM2 -> CCR1 = i++;
+		if(i > (TIM2 -> ARR))
+			dir = 0;
+	}
+	else
+	{
+		TIM2 -> CCR1 = i--;
+		if (i == 0)
+			dir = 1;
+	}
+}
+
